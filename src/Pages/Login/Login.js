728e9d470a844461a3aa8febeb9482d90fd19afa
Masum Billah <masum.monpura@gmail.com>
2022-11-02 20:30:21 +0600

    install firebase and set up local environment

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,42 +1,54 @@
-import React from 'react';
+import React from "react";
+import image from "../../assets/images/login/login.svg";
 
 const Login = () => {
-    return (
-        <div className="hero">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          
-          <div className="card  w-full shadow-2xl bg-base-100">
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="hero">
+      <div className="hero-content flex-col lg:flex-row-reverse justify-between">
+        <div className="card  w-full shadow-2xl bg-base-100">
           <div className="text-center">
             <h1 className="text-3xl text-center font-bold">Login now!</h1>
           </div>
-            <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
             </div>
-          </div>
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Login" />
+            </div>
+          </form>
+        </div>
+        <div className=" w-full">
+          <img src={image} alt="" />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
